4e457d5922e9e3394ee06048af688202e20007ef
Crawford <crawfordlee03@gmail.com>
2023-10-31 08:06:52 -0700

    Enabled auto reboot after upgrade on pi

--- Packages/packages.nix
+++ Packages/packages.nix
@@ -9,6 +9,7 @@
     ./Tailscale/tailscale.nix
     ./AdguardHome/adguardHome.nix
     ./Unbound/unbound.nix
+    ./Vaultwarden/vaultwarden.nix
   ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
