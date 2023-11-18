23e4f35327b6cb1134fe45ebb3a96539a4868c2f
= <=>
2023-11-17 20:38:52 -0800

    Valtwarden admin password moved to env file

--- Packages/Vaultwarden/vaultwarden.nix
+++ Packages/Vaultwarden/vaultwarden.nix
@@ -6,7 +6,7 @@
 		#environmentFile = "path to .env";
 		#Don't use plain text admin token and change password to something more secure (Probably fine if hosting only inside of a local network)
 		config = {
-			admin_token = "Crawford2003!";
+			admin_token = (builtins.readFile /etc/nixos/Env/ValtWardenPW.env);
 		};
 	};
 
